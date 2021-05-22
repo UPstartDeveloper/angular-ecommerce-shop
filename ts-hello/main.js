908d4cf80d9d4791200e32b4852de022b17e0dfb
= <zainr7989@gmail.com>
2021-05-22 18:09:46 -0400

    Add first TypeScript program and trainspile it.

---
+++ ts-hello/main.js
@@ -0,0 +1,6 @@
+/** Lesson 1: Typescript is just a superset of JS */
+var log = function (message) {
+    // all JS code can be run using the TS compiler
+    console.log(message);
+};
+log("Hello World!");
---
+++ ts-hello/main.ts
@@ -0,0 +1,8 @@
+/** Lesson 1: Typescript is just a superset of JS */
+
+let log = message => {
+    // all JS code can be run using the TS compiler
+    console.log(message);
+}
+
+log("Hello World!");
