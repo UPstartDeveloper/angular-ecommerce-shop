49d3291ad7ad42af631b5133fdede23a74f36058
= <zainr7989@gmail.com>
2021-05-31 11:37:42 -0400

    Use ngModel to make a 2-way form.

--- ecommerce-store/src/app/contact-form/contact-form.component.ts
+++ ecommerce-store/src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
+  currentVal = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  log() {
+    console.log(this.currentVal);
   }
 
 }
